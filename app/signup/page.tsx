7737dfc02cb75d1c497ff4e0b54e17fb2814a9ba
alphanoobie <hussaingadi.hg@gmail.com>
2023-08-27 13:36:09 +0530

    validate signup data

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,24 +6,35 @@ export default function SignUp() {
   const [password, setPassword] = useState("");
   const [cpassword, setCpassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState([] as string[]);
+
+  const validate = () => {
+    setErrors([]);
+    if (username.includes(" ")) {
+      setErrors((current) => [...current, "Username contains whitespace"]);
+    }
+    return errors
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     setLoading(true);
-    e.preventDefault();
-
-    // Post data to server
-    const response = await fetch("/api/signup", {
-      method: "POST",
-      body: JSON.stringify({ username: username, password: password }),
-    });
-    const data = await response.json();
+    const newErrors = validate();
+    if (newErrors.length === 0) {
+      e.preventDefault();
+      // Post data to server
+      const response = await fetch("/api/signup", {
+        method: "POST",
+        body: JSON.stringify({ username: username, password: password }),
+      });
+      const data = await response.json();
+    }
     setLoading(false);
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-150px)]">
       {loading ? <div>loading</div> : <div>NOT LOADING</div>}
+      {errors}
       <form className="flex flex-col" method="POST" onSubmit={handleSubmit}>
         <label htmlFor="username" className="text-lg">
           Username
