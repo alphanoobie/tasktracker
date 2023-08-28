6b4da8ce7cce54b7c72a50101c8bdb7e5846ec9e
alphanoobie <hussaingadi.hg@gmail.com>
2023-08-28 09:56:39 +0530

    create new task button

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useUserContext } from "./_context/user";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { authUser }:any = useUserContext();
+  const { authUser }: any = useUserContext();
 
   const router = useRouter();
   useEffect(() => {
@@ -12,5 +12,24 @@ export default function Home() {
       router.push("/signin");
     }
   }, []);
-  return <div>{JSON.stringify(authUser)}</div>;
+
+  const handleNewTask = () => {
+    console.log('create new task');
+  }
+  return (
+    <div>
+      <div className="w-[1000px] h-[100px] bg-[#6F8695] rounded-t-3xl flex items-center px-8">
+        <div>
+          <button
+            className="bg-green-700 w-12 h-12 rounded-full border-2 border-green-900 text-3xl flex items-center justify-center opacity-90 hover:opacity-100"
+            onClick={handleNewTask}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
