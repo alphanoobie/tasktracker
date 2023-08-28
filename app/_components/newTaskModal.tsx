cd1373abaaa6607952fbc5ed091a1cc4f9109ced
alphanoobie <hussaingadi.hg@gmail.com>
2023-08-28 14:19:05 +0530

    base modal to createw new task

---
+++ app/_components/newTaskModal.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const NewTaskModal = () => {
+  return (
+    <div className="fixed z-1 w-[700px] h-[500px] bg-[#6F8695] rounded-3xl p-8">hello</div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { useUserContext } from "./_context/user";
 import { useRouter } from "next/navigation";
+import { NewTaskModal } from "./_components/newTaskModal";
 
 export default function Home() {
   const { authUser }: any = useUserContext();
@@ -13,23 +14,32 @@ export default function Home() {
     }
   }, []);
 
-  const handleNewTask = () => {
-    console.log('create new task');
-  }
+  const handleNewTaskModal = async () => {
+    const data = await fetch("/api/createtask", {
+      method: "POST",
+      body: JSON.stringify({
+        user: authUser._id,
+      }),
+    });
+  };
   return (
     <div>
       <div className="w-[1000px] h-[100px] bg-[#6F8695] rounded-t-3xl flex items-center px-8">
         <div>
           <button
             className="bg-green-700 w-12 h-12 rounded-full border-2 border-green-900 text-3xl flex items-center justify-center opacity-90 hover:opacity-100"
-            onClick={handleNewTask}
+            onClick={handleNewTaskModal}
           >
             +
           </button>
         </div>
       </div>
 
-      <div></div>
+      <div className="flex justify-center m-4">
+        <NewTaskModal />
+      </div>
+
+      <div>{JSON.stringify(authUser)}</div>
     </div>
   );
 }
